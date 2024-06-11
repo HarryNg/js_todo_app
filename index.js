b629caf80a8a56570f05ead202994e0e0062bfa8
AA7776 <aa7776@student.jamk.fi>
2024-06-12 00:36:43 +0300

    Developed render Todo List task
    - Display the rendered list
    
    - Display checkbox and task desciption for each item

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let todoList = [];
 
 const addTodo = (todo) => {
   todoList.push(todo);
+  renderTodoList();
 }
+
 
 const addTodoForm = document.querySelector('#todoForm');
 
@@ -12,4 +14,33 @@ addTodoForm.addEventListener('submit', (event) => {
     const todo = todoInput.value;
     addTodo(todo);
     todoInput.value = '';
+});
+
+
+const renderTodoList = () => {
+    const todoListElement = document.querySelector('#todoList');
+    todoListElement.innerHTML = '';
+    todoList.forEach((todo,index) => {
+        const todoItem = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.addEventListener('change', (event) => {
+            if (event.target.checked) {
+            todoItem.style.textDecoration = 'line-through';
+            } else {
+            todoItem.style.textDecoration = 'none';
+            }
+        });
+
+        const todoDescription = document.createElement('label');
+        todoDescription.textContent = todo;
+        todoDescription.htmlFor = `task-${index}`;
+        todoItem.appendChild(checkbox);
+        todoItem.appendChild(todoDescription);
+        todoListElement.appendChild(todoItem);
+    });
+  }
+  
+document.addEventListener('DOMContentLoaded', () => {
+    renderTodoList();
 });
